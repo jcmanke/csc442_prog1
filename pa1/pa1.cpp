6aaad35fa71f1944bb25c6fa919a46c61de9822f
Joe Manke <joseph.manke@mines.sdsmt.edu>
2015-02-04 00:04:40 -0700

    Implemented Brightness, Gamma, and AutomatedConstrast functions

--- pa1/pa1.cpp
+++ pa1/pa1.cpp
@@ -1,4 +1,8 @@
 #include "pa1.h"
+#include <cmath>
+#include <vector>
+
+using namespace std;
 
 int main( int argc, char *argv[] )
 {
@@ -34,15 +38,15 @@ bool PointProcesses::Menu_Point_Grayscale(Image &image)
  */
 bool PointProcesses::Menu_Point_Negate(Image &image)
 {
-    for(unsigned int y = 0; y < image.Height(); y++)
+    for(uint y = 0; y < image.Height(); y++)
     {
-        for(unsigned int x = 0; x < image.Width(); x++)
+        for(uint x = 0; x < image.Width(); x++)
         {
             Pixel pixel = image[y][x];
 
-            int red = 255 - pixel.Red();
-            int blue = 255 - pixel.Blue();
-            int green = 255 - pixel.Green();
+            uchar red = 255 - pixel.Red();
+            uchar blue = 255 - pixel.Blue();
+            uchar green = 255 - pixel.Green();
 
             image[y][x].SetRGB(red, green, blue);
         }
@@ -60,9 +64,38 @@ bool PointProcesses::Menu_Point_Posterize(Image &image)
     return true;
 }
 
+/*
+ Author: Joe Manke
+
+ Adjusts an image's brightness by adding a value provided by the user to each
+    pixel's RGB values.
+ */
 bool PointProcesses::Menu_Point_Brightness(Image &image)
 {
-    return true;
+    int brighten = 20;
+
+    if(getParams(brighten))
+    {
+        for(uint y = 0; y < image.Height(); y++)
+        {
+            for(uint x = 0; x < image.Width(); x++)
+            {
+                Pixel pixel = image[y][x];
+
+                uchar red = qMin(pixel.Red() + brighten, 255);
+                uchar green = qMin(pixel.Green() + brighten, 255);
+                uchar blue = qMin(pixel.Blue() + brighten, 255);
+
+                image[y][x].SetRGB(red, green, blue);
+            }
+        }
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 bool PointProcesses::Menu_Point_Contrast(Image &image)
@@ -70,9 +103,33 @@ bool PointProcesses::Menu_Point_Contrast(Image &image)
     return true;
 }
 
+/*
+ Author: Joe Manke
+
+ Adjusts the image's intensity to by exponentiating each pixel's intensity
+    by a gamma value provided by the user.
+ */
 bool PointProcesses::Menu_Point_Gamma(Image &image)
 {
-    return true;
+    double gamma = 0.5;
+
+    if(getParams(gamma))
+    {
+        for(uint y = 0; y < image.Height(); y++)
+        {
+            for(uint x = 0; x < image.Width(); x++)
+            {
+                int intensity = pow(image[y][x].Intensity(), gamma);
+                image[y][x].SetIntensity(intensity);
+            }
+        }
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 bool PointProcesses::Menu_Point_Compress(Image &image)
@@ -101,8 +158,40 @@ bool PointProcesses::Menu_Point_DisplayHistogram(Image &image)
     return true;
 }
 
+/*
+ Author: Joe Manke
+
+ Performs an automated contrast stretch by scaling each pixel's intensity value
+    to relative to the minimum and maximum intensities of the entire image.
+ */
 bool PointProcesses::Menu_Point_AutomatedContrastStretch(Image &image)
 {
+    vector<uint> histogram = image.Histogram();
+
+    uint min = 0, max = 0;
+
+    //find minimum intensity
+    int i = 0;
+    while(histogram[i] == 0 && i < 256) i++;
+    min = i;
+
+    //find maximum intensity
+    i = 255;
+    while(histogram[i] == 0 && i >= 0) i--;
+    max = i;
+
+    double scale = 255.0 / (max - min);
+
+    for(uint y = 0; y < image.Height(); y++)
+    {
+        for(uint x = 0; x < image.Width(); x++)
+        {
+            int intensity = image[y][x].Intensity();
+            intensity = scale * (intensity - min);
+            image[y][x].SetIntensity(intensity);
+        }
+    }
+
     return true;
 }
 
